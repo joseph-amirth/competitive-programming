afde711dc018140449b8c00e017382c7d312bdd5
Joseph Amirtharaj <63185836+joseph-amirth@users.noreply.github.com>
2020-07-19 20:49:05 +0530

    Update fenwick-tree.cpp

--- data-structures/fenwick-tree.cpp
+++ data-structures/fenwick-tree.cpp
@@ -1,20 +1,22 @@
-template <typename T, typename F = function<T(const T&, const T&)>> class FenwickTree {
+template <typename T> class FenwickTree {
 public:
+    #define F function<T(const T&, const T&)>
+
     int n;
     vector<T> bit;
     F f;
 
     FenwickTree(): n(), bit(), f() {}
-    explicit FenwickTree(int _n, F func = plus<T>()): n(_n + 1), bit(_n + 1, 0), f(move(func)) {}
+    explicit FenwickTree(int n, F func = plus<T>()): n(n), bit(n + 1, 0), f(func) {}
 
     void add(int i, int val) {
-        while (i < n) {
+        while (i <= n) {
             bit[i] = f(bit[i], val);
             i += i & -i;
         }
     }
     T query(int i) {
-        int ans = 0;
+        T ans = T();
         while (i > 0) {
             ans = f(ans, bit[i]);
             i -= i & -i;
