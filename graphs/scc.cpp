3d07061f6d3ea4a9759c56ac16b6220f15176460
Joseph Amirtharaj <63185836+joseph-amirth@users.noreply.github.com>
2020-07-20 18:14:21 +0530

    Update scc.cpp

--- graphs/scc.cpp
+++ graphs/scc.cpp
@@ -1,40 +1,43 @@
+int f[N];
+bool visited[N];
+
 void computeSCC() {
     int s = 0, cur = 0;
-    function<void(int)> dfs1 = [&](int x) {
+    function<void(int)> dfs = [&](int x) {
         visited[x] = true;
 
         for (int &i : grev[x]) {
             if (!visited[i])
-                dfs1(i);
+                dfs(i);
         }
 
         cur += 1;
         f[cur] = x;
     };
 
-    function<void(int)> dfs2 = [&](int x) {
+    fill(visited, visited + n + 1, false);
+
+    for (int i = 1; i <= n; i++) {
+        if (!visited[i])
+            dfs(i);
+    }
+
+    dfs = [&](int x) {
         visited[x] = true;
         scc[s].push_back(x);
 
         for (int &i : g[x]) {
             if (!visited[i])
-                dfs2(i);
+                dfs(i);
         }
     };
-
-    fill(visited, visited + n + 1, false);
-
-    for (int i = 1; i <= n; i++) {
-        if (!visited[i])
-            dfs1(i);
-    }
 
     fill(visited, visited + n + 1, false);
 
     for (int i = n; i >= 1; i--) {
         if (!visited[f[i]]) {
             s = f[i];
-            dfs2(f[i]);
+            dfs(f[i]);
         }
     }
 }
