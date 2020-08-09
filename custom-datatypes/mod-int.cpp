0141800e377ddbd5d680de6d941c5657b831bd0e
Joseph Amirtharaj <63185836+joseph-amirth@users.noreply.github.com>
2020-08-10 02:15:40 +0530

    Update mod-int.cpp

--- custom-datatypes/mod-int.cpp
+++ custom-datatypes/mod-int.cpp
@@ -2,8 +2,8 @@ template <const int M> struct ModInt {
     int val;
     ModInt(ll x = 0): val((x % M + M) % M) {}
 
-    ModInt<M> pow(ll n) const {
-        ModInt<M> ans = 1, x = val;
+    ModInt pow(ll n) const {
+        ModInt ans = 1, x = val;
         while (n) {
             if (n & 1) ans *= x;
             x *= x;
@@ -12,7 +12,7 @@ template <const int M> struct ModInt {
         return ans;
     }
 
-    ModInt<M> inverse() const { return pow(M - 2); }
+    ModInt inverse() const { return pow(M - 2); }
     ModInt operator+ (const ModInt<M>& m) { return (val + m.val) % M; }
     ModInt operator- (const ModInt<M>& m) { return ((val - m.val) % M + M) % M; }
     ModInt operator* (const ModInt<M>& m) { return val * (ll) m.val % M; }
