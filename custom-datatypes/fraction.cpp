cbaea8f04eff4a1ab6bfd9a8cb4f6a024501c0d3
Joseph Amirtharaj <epicjoe128@gmail.com>
2020-08-10 17:29:41 +0530

    Update fraction.cpp

--- custom-datatypes/fraction.cpp
+++ custom-datatypes/fraction.cpp
@@ -1,27 +1,51 @@
 struct Fraction { //not fully functional (arithmetic operations haven't been implemented)
-    int num, dem;
-    explicit Fraction(int num = 0, int dem = 1) {
-        int g = __gcd(num, dem);
+    struct Fraction {
+    ll num, dem;
+    explicit Fraction(ll num = 0, ll dem = 1) {
+        if (dem < 0) {
+            dem *= -1;
+            num *= -1;
+        }
+        ll g = __gcd(num, dem);
         this->num = num / g;
         this->dem = dem / g;
     }
 
-    bool operator==(const Fraction& rhs) const {
-        return tie(num, dem) == tie(rhs.num, rhs.dem);
+    Fraction operator-() const {
+        Fraction res = *this;
+        res.num = -res.num;
+        return res;
     }
-    bool operator!=(const Fraction& rhs) const {
-        return !(*this == rhs);
+    Fraction operator+ (const Fraction& rhs) {
+        ll temp1 = num * rhs.dem + rhs.num * dem, temp2 = dem * rhs.dem;
+        ll g = __gcd(temp1, temp2);
+        return Fraction(temp1 / g, temp2 / g);
     }
-    bool operator<(const Fraction& rhs) const {
-        return (ll)num * rhs.dem < (ll)rhs.num * dem;
+    Fraction operator- (const Fraction& rhs) {
+        ll temp1 = num * rhs.dem - rhs.num * dem, temp2 = dem * rhs.dem;
+        ll g = __gcd(temp1, temp2);
+        return Fraction(temp1 / g, temp2 / g);
     }
-    bool operator>(const Fraction& rhs) const {
-        return rhs < *this;
+    Fraction operator* (const Fraction& rhs) {
+        ll temp1 = num * rhs.num, temp2 = dem * rhs.dem;
+        ll g = __gcd(temp1, temp2);
+        return Fraction(temp1 / g, temp2 / g);
     }
-    bool operator<=(const Fraction& rhs) const {
-        return !(*this > rhs);
+    Fraction operator/ (const Fraction& rhs) {
+        ll temp1 = num * rhs.dem, temp2 = dem * rhs.num;
+        ll g = __gcd(temp1, temp2);
+        return Fraction(temp1 / g, temp2 / g);
     }
-    bool operator>=(const Fraction& rhs) const {
-        return !(*this < rhs);
-    }
+    Fraction operator+=(const Fraction& rhs) { return *this = *this + rhs; }
+    Fraction operator-=(const Fraction& rhs) { return *this = *this - rhs; }
+    Fraction operator*=(const Fraction& rhs) { return *this = *this * rhs; }
+    Fraction operator/=(const Fraction& rhs) { return *this = *this / rhs; }
+
+    bool operator==(const Fraction& rhs) const { return tie(num, dem) == tie(rhs.num, rhs.dem); }
+    bool operator!=(const Fraction& rhs) const { return !(*this == rhs); }
+    bool operator< (const Fraction& rhs) const { return num * rhs.dem < rhs.num * dem; }
+    bool operator> (const Fraction& rhs) const { return rhs < *this; }
+    bool operator<=(const Fraction& rhs) const { return !(*this > rhs);}
+    bool operator>=(const Fraction& rhs) const { return !(*this < rhs); }
+};
 };
