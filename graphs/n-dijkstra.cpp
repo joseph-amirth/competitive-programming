5e5e0c1ad2373d03159b962833930e4f0ebdde17
Joseph Amirtharaj <63185836+joseph-amirth@users.noreply.github.com>
2020-08-10 13:05:06 +0530

    Update n-dijkstra.cpp

--- graphs/n-dijkstra.cpp
+++ graphs/n-dijkstra.cpp
@@ -1,31 +1,32 @@
 int d[N][N];
 bool visited[N];
 
-void nDijkstra(int s) {
-    function<void(int)> dijkstra = [&](int s) {
-        fill(visited, visited + n + 1, false);
-        fill(d[s], d[s] + n + 1, inf);
+void Dijkstra(int s = 1) {
+    fill(visited, visited + n + 1, false);
+    fill(d[s], d[s] + n + 1, inf);
 
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> q;
-        q.push({0, s});
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> q;
+    q.push({0, s});
 
-        d[s][s] = 0;
-        while (!q.empty()) {
-            int u = q.top().second;
-            q.pop();
+    d[s][s] = 0;
+    while (!q.empty()) {
+        int u = q.top().second;
+        q.pop();
 
-            if (visited[u])
-                continue;
-            visited[u] = true;
+        if (visited[u])
+            continue;
+        visited[u] = true;
 
-            for (pair<int, int> &i : g[u]) {
-                if (d[s][i.first] > d[s][u] + i.second) {
-                    d[s][i.first] = d[s][u] + i.second;
-                    q.push({d[s][i.first], i.first});
-                }
+        for (auto &[v, w] : g[u]) {
+            if (d[s][v] > d[s][u] + w) {
+                d[s][v] = d[s][u] + w;
+                q.push({d[s][v], v});
             }
         }
-    };
+    }
+}
+
+void nDijkstra() { //apsp
     for (int i = 1; i <= n; i++)
         dijkstra(i);
 }
