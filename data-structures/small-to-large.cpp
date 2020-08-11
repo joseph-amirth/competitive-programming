79c466873137430b0e05f0576f72031db743d990
Joseph Amirtharaj <63185836+joseph-amirth@users.noreply.github.com>
2020-08-11 12:51:38 +0530

    Create small-to-large.cpp

---
+++ data-structures/small-to-large.cpp
@@ -0,0 +1,33 @@
+class UnionFind { //union find but with small to large merging
+public:
+    int n;
+    vector<int> p;
+    vector<vector<int>> cmps;
+
+    explicit UnionFind(int n = 0): n(n), p(n + 1), cmps(n + 1) {
+        iota(all(p), 0);
+        for (int i = 1; i <= n; i++) cmps[i] = {i};
+    }
+    int size() const {
+        return n;
+    }
+    int find(int x) {
+        return p[x];
+    }
+    bool merge(int x, int y) {
+        int s1 = find(x);
+        int s2 = find(y);
+
+        if (s1 == s2)
+            return false;
+        if (cmps[s2].size() > cmps[s1].size())
+            swap(s1, s2);
+        for (int i : cmps[s2]) {
+            cmps[s1].push_back(i);
+            p[i] = s1;
+        } cmps[s2].clear();
+
+        n--;
+        return true;
+    }
+};
