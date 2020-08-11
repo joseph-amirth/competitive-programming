989a8d57ded312b35a4491a99fabe0dcedff1865
Joseph Amirtharaj <63185836+joseph-amirth@users.noreply.github.com>
2020-08-11 15:27:58 +0530

    Update sqrt-primality-test.cpp

--- miscellaneous/sqrt-primality-test.cpp
+++ miscellaneous/sqrt-primality-test.cpp
@@ -2,7 +2,7 @@ bool isPrime(int x) {
     if (x == 1)
         return false;
 
-    for (int i = 1; i * i <= x; i++) {
+    for (int i = 2; i * i <= x; i++) {
         if (x % i == 0)
             return false;
     }
