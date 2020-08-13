a8d4419a5299954ab9d2bbbff1690b162bb27af3
Joseph Amirtharaj <63185836+joseph-amirth@users.noreply.github.com>
2020-08-14 00:05:23 +0530

    Update seg-tree.cpp

--- data-structures/seg-tree.cpp
+++ data-structures/seg-tree.cpp
@@ -1,18 +1,18 @@
 template <typename T> class SegmentTree {
 public:
-    #define F function<T(const T&, const T&)>
+    using F = function<T(const T&, const T&)>;
 
     int n;
-    vector<T> t; 
+    vector<T> t;
     T e;
     F f;
     SegmentTree() : n(), t(), e(), f() {}
 
-    template <typename U> void build(const U& arr, int _n, T _e = T(), F func = plus<>()) {
+    template <typename U> void build(const U& arr, int _n, T _e, F _f) {
         n = _n;
         t.resize(4 * n + 4);
         e = _e;
-        f = func;
+        f = _f;
         build(arr, 1, 0, n - 1);
     }
 
@@ -29,11 +29,11 @@ public:
         t[i] = f(t[i << 1], t[i << 1 | 1]);
     }
 
-    template <typename U> void update(int idx, U val) {
+    template <typename U> void update(const int& idx, const U& val) {
         update(idx, val, 1, 0, n - 1);
     }
 
-    template <typename U> void update(int idx, U val, int i, int l, int r) {
+    template <typename U> void update(const int& idx, const U& val, int i, int l, int r) {
         if (l == r) {
             t[i] = T(val);
             return;
@@ -53,7 +53,7 @@ public:
     T query(int ql, int qr, int i, int l, int r) {
         if (ql > r or qr < l)
             return e;
-        if (l >= ql and r <= qr)
+        if (ql <= l and r <= qr)
             return t[i];
         int mid = (l + r) >> 1;
         T x = query(ql, qr, i << 1, l, mid), y = query(ql, qr, i << 1 | 1, mid + 1, r);
