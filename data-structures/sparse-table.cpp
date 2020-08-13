fa4326102f3571a57f698585aeb0972495151d1b
Joseph Amirtharaj <63185836+joseph-amirth@users.noreply.github.com>
2020-08-13 12:38:01 +0530

    Update sparse-table.cpp

--- data-structures/sparse-table.cpp
+++ data-structures/sparse-table.cpp
@@ -1,34 +1,22 @@
-template <typename T, bool I = true> class SparseTable {
+template <typename T> class SparseTable {
 public:
     #define F function<T(const T&, const T&)>
 
     int n;
     vector<vector<T>> mat;
-    vector<int> log;
-    T e;
     F f;
-    SparseTable(): n(), mat(), log(), e(), f() {}
+    SparseTable(): n(), mat(), f() {}
 
-    template <typename U> void build(const U& arr, int _n, T _e, F func) {
+    template <typename U> void build(const U& arr, int _n, F _f) {
         n = _n;
-        e = _e;
-        f = func;
+        f = _f;
         mat.resize((int)log2(n) + 1);
-
-        if (I) {
-            log.resize(n + 1);
-            log[1] = 0;
-            for (int i = 2; i <= n; i++) {
-                log[i] = 1 + log[i >> 1];
-            }
-        }
-
         mat[0].resize(n);
         for (int i = 0; i < n; i++)
             mat[0][i] = T(arr[i]);
-
+        
         for (int j = 1; j < mat.size(); j++) {
-            mat[j].resize(n - j);
+            mat[j].resize(n - (1 << j) + 1);
             for (int i = 0; i + (1 << j) <= n; i++) {
                 mat[j][i] = f(mat[j - 1][i], mat[j - 1][i + (1 << (j - 1))]);
             }
@@ -36,18 +24,7 @@ public:
     }
 
     T query(int l, int r) {
-        if (I) {
-            int j = log[r - l + 1];
-            return f(mat[j][l], mat[j][r + 1 - (1 << j)]);
-        }
-
-        T ans = e;
-        for (int j = mat.size() - 1; j >= 0; j--) {
-            if ((1 << j) <= r - l + 1) {
-                ans = f(ans, mat[j][l]);
-                l += (1 << j);
-            }
-        }
-        return ans;
+        int j = 32 - __builtin_clz(r - l + 1) - 1;
+        return f(mat[j][l], mat[j][r + 1 - (1 << j)]);
     }
 };
