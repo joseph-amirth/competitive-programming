a91e070fd1860eee35dac159951bb3de407af1e4
Joseph Amirtharaj <63185836+joseph-amirth@users.noreply.github.com>
2020-08-18 11:05:52 +0530

    Update dijkstra.cpp

--- graphs/dijkstra.cpp
+++ graphs/dijkstra.cpp
@@ -1,14 +1,14 @@
-int d[N];
+int dist[N];
 bool visited[N];
 
 void Dijkstra(int s = 1) {
     fill(visited, visited + n + 1, false);
-    fill(d, d + n + 1, inf);
+    fill(dist, dist + n + 1, inf);
 
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> q;
-    q.push({0, s});
+    q.emplace(0, s);
 
-    d[s] = 0;
+    dist[s] = 0;
     while (!q.empty()) {
         int u = q.top().second;
         q.pop();
@@ -18,9 +18,9 @@ void Dijkstra(int s = 1) {
         visited[u] = true;
 
         for (auto &[v, w] : g[u]) {
-            if (d[v] > d[u] + w) {
-                d[v] = d[u] + w;
-                q.push({d[v], v});
+            if (dist[v] > dist[u] + w) {
+                dist[v] = dist[u] + w;
+                q.emplace(dist[v], v);
             }
         }
     }
