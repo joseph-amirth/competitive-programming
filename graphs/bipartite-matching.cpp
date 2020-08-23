829c460f715bedd69fb78134004dcc010b2efa48
Joseph Amirtharaj <63185836+joseph-amirth@users.noreply.github.com>
2020-08-23 17:40:59 +0530

    Create bipartite-matching.cpp

---
+++ graphs/bipartite-matching.cpp
@@ -0,0 +1,42 @@
+class Matching {
+public:
+    Graph g;
+    vector<int> c, match;
+    vector<bool> visited;
+
+    Matching(Graph& g): g(g), c(g.n, -1), match(g.n, -1), visited(g.n) {}
+
+    void dfs(int x, int h) {
+        c[x] = h;
+        for (int &i : g[x]) {
+            if (c[i] == -1)
+                dfs(i, h ^ 1);
+        }
+    }
+    bool dfs(int x) {
+        for (int &i : g[x]) {
+            if (!visited[i]) {
+                visited[i] = true;
+                if (match[i] < 0 or dfs(match[i])) {
+                    match[i] = x;
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    int size() {
+        for (int i = 0; i < g.n; i++) {
+            if (c[i] == -1)
+                dfs(i, 0);
+        }
+        int matching = 0;
+        for (int x = 0; x < g.n; x++) {
+            if (!c[x]) {
+                fill(visited.begin(), visited.end(), false);
+                matching += dfs(x);
+            }
+        }
+        return matching;
+    }
+};
