dbbc465a36383f4d99b6cd679ab57612f9ca63ae
Joseph Amirtharaj <63185836+joseph-amirth@users.noreply.github.com>
2020-08-23 20:33:57 +0530

    Update kmp.cpp

--- strings/kmp.cpp
+++ strings/kmp.cpp
@@ -1,47 +1,39 @@
-int lps[N];
-void constructLps(const string &t) {
-    int m = t.length();
+template <typename T>
+vector<int> KMP(const T& s, int n, const T& t, int m) {
+    vector<int> lps; {
+        int len = 0, i = 1;
+        lps[0] = 0;
 
-    int len = 0, i = 1;
-    lps[0] = 0;
-
-    while (i < m) {
-        if (t[i] == t[len]) {
-            len += 1;
-            lps[i] = len;
-            i += 1;
-        }
-        else if (len != 0) {
-            len = lps[len - 1];
-        }
-        else {
-            lps[i] = 0;
-            i += 1;
+        while (i < m) {
+            if (t[i] == t[len]) {
+                len += 1;
+                lps[i] = len;
+                i += 1;
+            } else if (len != 0) {
+                len = lps[len - 1];
+            } else {
+                lps[i] = 0;
+                i += 1;
+            }
         }
     }
-}
 
-vector<int> kmp(const string &s, const string &t) {
-    constructLps(t);
-    vector<int> occur;
-
-    int n = s.length(), m = t.length(), i = 0, j = 0;
+    vector<int> match;
+    int i = 0, j = 0;
 
     while (i < n) {
         if (s[i] == t[j]) {
             i += 1;
             j += 1;
         }
-
         if (j == m) {
-            occur.push_back(i - j);
+            match.push_back(i - j);
             j = lps[j - 1];
-        }
-        else if (i < n and s[i] != t[j]) {
+        } else if (i < n and s[i] != t[j]) {
             if (j != 0)
-                j = lps[j-1];
+                j = lps[j - 1];
             else i += 1;
         }
     }
-    return occur;
-}
+    return match;
+};
