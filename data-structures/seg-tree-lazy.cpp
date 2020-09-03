d045b6044d85adab22046748f15a1e96dab2e4db
Joseph Amirtharaj <63185836+joseph-amirth@users.noreply.github.com>
2020-09-03 23:04:12 +0530

    Update seg-tree-lazy.cpp

--- data-structures/seg-tree-lazy.cpp
+++ data-structures/seg-tree-lazy.cpp
@@ -1,4 +1,5 @@
-template <typename T> class SegmentTree {
+template <typename T>
+class SegmentTree {
 public:
     using F = function<T(const T&, const T&)>;
 
@@ -9,16 +10,13 @@ public:
     F f;
     SegmentTree() : n(), t(), lazy(), e(), f() {}
 
-    template <typename U> void build(const U& arr, int _n, T _e, F _f) {
-        n = _n;
-        t.resize(4 * n + 4);
-        lazy.resize(4 * n + 4);
-        e = _e;
-        f = _f;
+    template <typename U>
+    SegmentTree(const U& arr, int n, T e, F f): n(n), t(4 * n), lazy(4 * n), e(e), f(f) {
         build(arr, 1, 0, n - 1);
     }
 
-    template <typename U> void build(const U& arr, int i, int l, int r) {
+    template <typename U>
+    void build(const U& arr, int i, int l, int r) {
         if (l == r) {
             t[i] = T(arr[l]);
             return;
@@ -35,11 +33,13 @@ public:
         //lazy
     }
 
-    template <typename U> void update(int idx, U val) {
-        update(idx, val, 1, 0, n - 1);
+    template <typename U>
+    void update(int idx, U val) {
+        updateHelper(idx, val, 1, 0, n - 1);
     }
 
-    template <typename U> void update(int idx, U val, int i, int l, int r) {
+    template <typename U>
+    void updateHelper(int idx, U val, int i, int l, int r) {
         if (l == r) {
             t[i] = T(val);
             return;
@@ -47,17 +47,19 @@ public:
         push(i, l, r);
         int mid = (l + r) >> 1;
         if (idx <= mid)
-            update(idx, val, i << 1, l, mid);
-        else update(idx, val, i << 1 | 1, mid + 1, r);
+            updateHelper(idx, val, i << 1, l, mid);
+        else updateHelper(idx, val, i << 1 | 1, mid + 1, r);
 
         t[i] = f(t[i << 1], t[i << 1 | 1]);
     }
 
-    template <typename U> void update(int ql, int qr, U val) {
-        update(ql, qr, val, 1, 0, n - 1);
+    template <typename U>
+    void update(int ql, int qr, U val) {
+        updateHelper(ql, qr, val, 1, 0, n - 1);
     }
 
-    template <typename U> void update(int ql, int qr, U val, int i, int l, int r) {
+    template <typename U>
+    void updateHelper(int ql, int qr, U val, int i, int l, int r) {
         if (ql > r or qr < l)
             return;
         if (ql <= l and r <= qr) {
@@ -66,24 +68,24 @@ public:
         }
         push(i, l, r);
         int mid = (l + r) >> 1;
-        update(ql, qr, val, i << 1, l, mid);
-        update(ql, qr, val, i << 1 | 1, mid + 1, r);
+        updateHelper(ql, qr, val, i << 1, l, mid);
+        updateHelper(ql, qr, val, i << 1 | 1, mid + 1, r);
 
         t[i] = f(t[i << 1], t[i << 1 | 1]);
     }
 
     T query(int ql, int qr) {
-        return query(ql, qr, 1, 0, n - 1);
+        return queryHelper(ql, qr, 1, 0, n - 1);
     }
 
-    T query(int ql, int qr, int i, int l, int r) {
+    T queryHelper(int ql, int qr, int i, int l, int r) {
         if (ql > r or qr < l)
             return e;
         if (ql <= l and r <= qr)
             return t[i];
         push(i, l, r);
         int mid = (l + r) >> 1;
-        T x = query(ql, qr, i << 1, l, mid), y = query(ql, qr, i << 1 | 1, mid + 1, r);
+        T x = queryHelper(ql, qr, i << 1, l, mid), y = queryHelper(ql, qr, i << 1 | 1, mid + 1, r);
         return f(x, y);
     }
 };
