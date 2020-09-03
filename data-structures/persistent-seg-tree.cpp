eec4508fbcb5f1444bffb78252089d51e08fa5db
Joseph Amirtharaj <63185836+joseph-amirth@users.noreply.github.com>
2020-09-03 23:12:00 +0530

    Update persistent-seg-tree.cpp

--- data-structures/persistent-seg-tree.cpp
+++ data-structures/persistent-seg-tree.cpp
@@ -1,71 +1,78 @@
-template <typename T> class SegmentTree {
+template <typename T>
+class SegmentTree {
 public:
     using F = function<T(const T&, const T&)>;
 
-    struct node {
+    struct Node {
         T val;
-        node *l, *r;
-        explicit node(T val) : val(val), l(nullptr), r(nullptr) {}
-        node(T val, node* l, node* r): val(val), l(l), r(r) {}
+        Node *l, *r;
+        explicit Node(T val) : val(val), l(nullptr), r(nullptr) {}
+        Node(T val, Node* l, Node* r): val(val), l(l), r(r) {}
     };
 
     int n;
-    vector<node*> root;
-    node* ver;
+    vector<Node*> root;
+    Node* curVer;
     T e;
     F f;
-    SegmentTree(): root(), n(), ver(), e(), f() {}
+    SegmentTree(): root(), n(), curVer(), e(), f() {}
 
-    template <typename U> void build(const U& arr, int _n, T _e, F _f) {
-        n = _n;
-        e = _e;
-        f = _f;
-        root.push_back(build(arr, 0, n - 1));
-        ver = root.back();
+    void changeVer(int k) {
+        assert(k >= 1 and k <= root.size());
+        curVer = root[k - 1];
     }
 
-    template <typename U> node* build(const U& arr, int l, int r) {
+    template <typename U>
+    SegmentTree(const U& arr, int n, T e, F f): n(n), e(e), f(f) {
+        root.push_back(build(arr, 0, n - 1));
+        curVer = root.back();
+    }
+
+    template <typename U>
+    Node* build(const U& arr, int l, int r) {
         if (l == r) {
-            node* temp = new node(T(arr[l]));
+            Node* temp = new Node(T(arr[l]));
             return temp;
         }
 
         int mid = (l + r) >> 1;
-        node* temp1 = build(arr, l, mid);
-        node* temp2 = build(arr, mid + 1, r);
-        return new node(f(temp1->val, temp2->val), temp1, temp2);
+        Node* temp1 = build(arr, l, mid);
+        Node* temp2 = build(arr, mid + 1, r);
+        return new Node(f(temp1->val, temp2->val), temp1, temp2);
     }
 
-    template <typename U> void update(int idx, U val) {
-        root.push_back(update(idx, val, ver, 0, n - 1));
-        ver = root.back();
+    template <typename U>
+    void update(int idx, U val) {
+        root.push_back(updateHelper(idx, val, curVer, 0, n - 1));
+        curVer = root.back();
     }
 
-    template <typename U> node* update(int idx, U val, node* v, int l, int r) {
+    template <typename U>
+    Node* updateHelper(int idx, U val, Node *v, int l, int r) {
         if (l == r)
-            return new node(T(val));
+            return new Node(T(val));
 
         int mid = (l + r) >> 1;
         if (idx <= mid) {
-            node* temp = update(idx, val, v->l, l, mid);
-            return new node(f(temp->val, v->r->val), temp, v->r);
+            Node *temp = updateHelper(idx, val, v->l, l, mid);
+            return new Node(f(temp->val, v->r->val), temp, v->r);
         } else {
-            node *temp = update(idx, val, v->r, mid + 1, r);
-            return new node(f(v->l->val, temp->val), v->l, temp);
+            Node *temp = updateHelper(idx, val, v->r, mid + 1, r);
+            return new Node(f(v->l->val, temp->val), v->l, temp);
         }
     }
 
     T query(int ql, int qr) {
-        return query(ql, qr, ver, 0, n - 1);
+        return queryHelper(ql, qr, curVer, 0, n - 1);
     }
 
-    T query(int ql, int qr, node* v, int l, int r) {
+    T queryHelper(int ql, int qr, Node *v, int l, int r) {
         if (ql > r or l > qr)
             return e;
         if (ql <= l and r <= qr)
             return v->val;
 
         int mid = (l + r) >> 1;
-        return f(query(ql, qr, v->l, l, mid), query(ql, qr, v->r, mid + 1, r));
+        return f(queryHelper(ql, qr, v->l, l, mid), queryHelper(ql, qr, v->r, mid + 1, r));
     }
 };
