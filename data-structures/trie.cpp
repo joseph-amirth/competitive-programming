e211ff8991dcfbc529bc9a52e4efb29f32b5f4fc
Joseph Amirtharaj <63185836+joseph-amirth@users.noreply.github.com>
2020-09-03 20:53:15 +0530

    Update trie.cpp

--- data-structures/trie.cpp
+++ data-structures/trie.cpp
@@ -1,7 +1,6 @@
 struct Node {
     Node* arr[26];
     bool isEnd;
-    Node() : arr{}, isEnd() {}
 };
 
 class Trie {
@@ -9,7 +8,7 @@ public:
     Node *root;
     Trie(): root(new Node()) {}
 
-    void insert (const string& s) const {
+    void insert (const string& s) {
         Node *curNode = root;
         for (char ch : s) {
             if (!curNode->arr[ch - 'a'])
@@ -19,7 +18,7 @@ public:
         curNode->isEnd = true;
     }
 
-    bool search(const string& s) const {
+    bool search(const string& s) {
         Node *curNode = root;
         for (char ch : s) {
             if (!curNode->arr[ch - 'a'])
@@ -27,5 +26,18 @@ public:
             curNode = curNode->arr[ch - 'a'];
         }
         return (curNode != nullptr and curNode->isEnd);
+    }
+
+    ~Trie() {
+        if (root != nullptr) {
+            function<void(Node*)> dfs = [&] (Node *x) {
+                for (int i = 0; i < 26; i++) {
+                    if (x->arr[i])
+                        dfs(x->arr[i]);
+                }
+                delete x;
+            };
+            dfs(root);
+        }
     }
 };
