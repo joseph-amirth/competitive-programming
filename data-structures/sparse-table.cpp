f20b5f8cc706eb58d542ce068da583d22d26eeb8
Joseph Amirtharaj <63185836+joseph-amirth@users.noreply.github.com>
2020-09-03 23:54:38 +0530

    Update sparse-table.cpp

--- data-structures/sparse-table.cpp
+++ data-structures/sparse-table.cpp
@@ -1,4 +1,5 @@
-template <typename T> class SparseTable {
+template <typename T>
+class SparseTable {
 public:
     using F = function<T(const T&, const T&)>;
 
@@ -7,14 +8,11 @@ public:
     F f;
     SparseTable(): n(), mat(), f() {}
 
-    template <typename U> void build(const U& arr, int _n, F _f) {
-        n = _n;
-        f = _f;
-        mat.resize((int)log2(n) + 1);
+    template <typename U>
+    SparseTable(const U& arr, int n, F f): n(n), f(f), mat((int)log2(n) + 1) {
         mat[0].resize(n);
         for (int i = 0; i < n; i++)
             mat[0][i] = T(arr[i]);
-
         for (int j = 1; j < mat.size(); j++) {
             mat[j].resize(n - (1 << j) + 1);
             for (int i = 0; i + (1 << j) <= n; i++) {
@@ -22,7 +20,6 @@ public:
             }
         }
     }
-
     T query(int l, int r) {
         int j = 32 - __builtin_clz(r - l + 1) - 1;
         return f(mat[j][l], mat[j][r + 1 - (1 << j)]);
